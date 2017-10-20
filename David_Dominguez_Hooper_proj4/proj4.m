68129d54463cf5c609cf4afdcc2cfc7072c881b2
David Dominguez Hooper <davidhooper4@berkeley.edu>
2017-10-19 22:58:16 -0700

    got mean face working

--- David_Dominguez_Hooper_proj4/proj4.m
+++ David_Dominguez_Hooper_proj4/proj4.m
@@ -10,29 +10,51 @@ im2 = imresize(im2double(imread(imname{2})),[300 300]);
 
 % Part 1: Defining Correspondences
 
-load('movingPoints.mat', 'movingPoints');
-load('fixedPoints.mat', 'fixedPoints');
+load('im1_pts.mat', 'im1_pts');
+load('im2_pts.mat', 'im2_pts');
 
-% h  = cpselect(im1,im2, movingPoints, fixedPoints);
-% 
-% save('movingPoints.mat', 'movingPoints');
-% save('fixedPoints.mat', 'fixedPoints');
+% h  = cpselect(im1,im2, im1_pts, im2_pts);
+% save('im1_pts.mat', 'im1_pts');
+% save('im2_pts.mat', 'im2_pts');
 
-
-
-%     height = floor(size(fullim,1)/3);
-%     width = floor(size(fullim,2));
 
 % PART 2: Computing the "Mid-way Face"
-tri1_pts = delaunayTriangulation(movingPoints);
-tri2_pts = delaunayTriangulation(fixedPoints);
+mid_pts = (im1_pts + im2_pts)/2;
+tri = delaunay(mid_pts); % triangle pts for both images
 
-figure, triplot(tri1_pts);
-set(gca,'Ydir','reverse')
-figure, triplot(tri2_pts);
-set(gca,'Ydir','reverse')
+% figure, set(gca,'Ydir','reverse'), triplot(tri1_pts);
+% figure, set(gca,'Ydir','reverse'), triplot(tri2_pts);
+% figure, set(gca,'Ydir','reverse'), triplot(mid_pts);
 
-A = computeAffine(tri1_pts,tri2_pts);
+
+num_tris = length(tri);
+aff_trans_matrices = cell(num_tris, 1);
+
+tri1 = delaunay(im1_pts);
+for i = 1:num_tris
+    aff_trans_matrices{i} = computeAffine(tri1(i, :), tri(i, :), im1_pts, mid_pts);
+end
+
+[h, w, channel] = size(im1);
+[Y, X] = meshgrid(1:h, 1:w);
+[YN, XN] = meshgrid(1:h, 1:w);
+t = mytsearch(mid_pts(:,1), mid_pts(:,2), tri, Y, X);
+
+
+for i = 1:h %y
+    for j = 1:w %x
+        t_m = aff_trans_matrices{t(i, j)};
+        new = t_m*([j, i, 1].');
+        YN(i, j) = new(1, :);
+        XN(i, j) = new(2, :);
+    end
+end
+r = interp2(Y,X,im1(:, :, 1),YN,XN,'linear');
+g = interp2(Y,X,im1(:, :, 2),YN,XN,'linear');
+b = interp2(Y,X,im1(:, :, 3),YN,XN,'linear');
+figure, imshow(cat(3, r, g, b));
+
+% morphed_im = morph(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve_frac);
 
 
 % PART 3: The Morph Sequence
@@ -66,8 +88,21 @@ morphed_im = morph(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve_frac);
 
 % Functions:
 
-function A = computeAffine(tri1_pts,tri2_pts)
-    A = 0;
+function A = computeAffine(tri1_pts,tri2_pts, xy1_pts, xy2_pts)
+    r1 = [xy1_pts(tri1_pts(1, 1), :), 1];
+    r2 = [xy1_pts(tri1_pts(1, 2), :), 1];
+    r3 = [xy1_pts(tri1_pts(1, 3), :), 1];
+    x = vertcat(r1, r2, r3).';
+    
+    r1 = [xy2_pts(tri2_pts(1, 1), :), 1];
+    r2 = [xy2_pts(tri2_pts(1, 2), :), 1];
+    r3 = [xy2_pts(tri2_pts(1, 3), :), 1];
+    
+    b = vertcat(r1, r2, r3).';
+    
+    A = b/x;
+    A(3, :) = [0, 0, 1];
+    
 end
 
 function morphed_im = morph(im1, im2, im1_pts, im2_pts, tri, warp_frac, dissolve_frac);
